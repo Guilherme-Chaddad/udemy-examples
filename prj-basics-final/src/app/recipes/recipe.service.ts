2311da55d76ce52344286778dba5ddc16609d33e
Rufino <Guilherme.Rufino@mastertest.com>
2018-02-20 10:27:05 -0600

    Lecture 111

--- prj-basics-final/src/app/recipes/recipe.service.ts
+++ prj-basics-final/src/app/recipes/recipe.service.ts
@@ -1,17 +1,38 @@
 
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService{
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('A Test Recipe2 ', 'This is simply a test2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('A Test Recipe',
+         'This is simply a test',
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+            new Ingredient('meat', 1), new Ingredient('bacon', 10)
+        ]),
+        new Recipe('A Test Recipe2 ',
+         'This is simply a test2',
+          'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+            new Ingredient('meat', 1), new Ingredient('bacon', 10)
+        ])
     ];
+
+    constructor(private slService: ShoppingListService){
+
+    }
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
 }
