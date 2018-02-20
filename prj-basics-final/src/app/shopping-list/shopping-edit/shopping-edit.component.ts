12ba7f9517a2135b74e78e566125d018204d480e
Rufino <Guilherme.Rufino@mastertest.com>
2018-02-20 08:43:40 -0600

    Lecture 109

--- prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ prj-basics-final/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,14 +10,15 @@ import { Ingredient } from '../../shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') inputNameRef: ElementRef;
   @ViewChild('amountInput') inputAmountRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
-  constructor() { }
+  //@Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   } 
 
   onAddItem(){
     const ingredient = new Ingredient(this.inputNameRef.nativeElement.value, this.inputAmountRef.nativeElement.value);
-    this.ingredientAdded.emit(ingredient);
+    //this.ingredientAdded.emit(ingredient);
+    this.slService.addIngredient(ingredient);
   }
 }
