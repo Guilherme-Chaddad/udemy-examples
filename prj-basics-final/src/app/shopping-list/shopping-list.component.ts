dcff38f13fc00effdb443d95110937e31ac938c4
e078846 <guilherme.rufino@mastercard.com>
2018-02-23 12:23:33 -0600

    Lecture 209

--- prj-basics-final/src/app/shopping-list/shopping-list.component.ts
+++ prj-basics-final/src/app/shopping-list/shopping-list.component.ts
@@ -25,4 +25,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ngOnDestroy(){
     this.slSubscription.unsubscribe();
   }
+
+  onEditItem(index: number){
+    this.shoppingListService.startedEditing.next(index);
+  }
 }
