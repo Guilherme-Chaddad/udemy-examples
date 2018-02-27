1b269c7c6512e70d094d072dfe1b8bcb6a7f6939
Guilherme Chaddad <guichaddad@gmail.com>
2018-02-27 19:36:45 -0300

    SpringHibernate Lecture 58

---
+++ SpringHibernateCourse/anotations-demo/src/com/annotations/demo/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.annotations.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
