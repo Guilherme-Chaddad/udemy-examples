48994681b5215640d9f08e907e32c98c55066845
e078846 <guilherme.rufino@mastercard.com>
2018-04-20 11:55:28 -0500

    Angular Assessment 6

---
+++ AngularCourse/forms-reactive-assignment-start/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidator } from './custom-validator';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  projectForm: FormGroup;
+
+  ngOnInit(){
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required, CustomValidator.invalidProjectName],
+         CustomValidator.asyncInvalidProjectName),
+      'email': new FormControl(null, [Validators.required,Validators.email]),
+      'projectStatus': new FormControl('critical')
+    });
+  }
+
+  onProjectSave(){
+    console.log(this.projectForm.value);
+  }
+}
