3bddcf08cba92bccf5c953dc56dcee88e4788c9e
e078846 <guilherme.rufino@mastercard.com>
2018-04-20 12:46:34 -0500

    Angular Lecture 221

--- AngularCourse/prj-basics-final/src/app/recipes/recipe.service.ts
+++ AngularCourse/prj-basics-final/src/app/recipes/recipe.service.ts
@@ -3,10 +3,11 @@ import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs/Subject";
 
 @Injectable()
 export class RecipeService{
-
+    recipeChanges = new Subject<Recipe[]>();
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe',
          'This is simply a test',
@@ -36,5 +37,20 @@ export class RecipeService{
 
     addIngredientsToShoppingList(ingredients: Ingredient[]){
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe){
+        this.recipes.push(recipe);
+        this.recipeChanges.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe){
+        this.recipes[index] = newRecipe;
+        this.recipeChanges.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number){
+        this.recipes.splice(index, 1);
+        this.recipeChanges.next(this.recipes.slice());
     }
 }
