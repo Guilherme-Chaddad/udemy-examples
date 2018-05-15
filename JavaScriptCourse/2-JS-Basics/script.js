b6e1825c4185e9492b669c56bb503589926f5f1f
Guilherme Chaddad <guichaddad@gmail.com>
2018-05-15 20:20:36 -0300

    JS Lecture 10

---
+++ JavaScriptCourse/2-JS-Basics/script.js
@@ -0,0 +1,40 @@
+
+//Variables
+/*var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(name+' '+lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);*/
+
+//Variables 2
+var name = 'John';
+var age = 26;
+
+console.log(name+age);
+console.log(age+age);
+
+var job, isMarried;
+
+console.log(job);
+
+job = 'System Analyst';
+isMarried = false;
+
+console.log(name+' '+age+ ' '+job+' '+isMarried);
+
+age = 'thirty six';
+job = 'driver';
+
+console.log(name+' '+age+ ' '+job+' '+isMarried);
+
+var lastName = prompt('What is the last name?');
+
+console.log(lastName);
+
+alert(name+' '+age+ ' '+job+' '+isMarried);
