f40e3183ef0a39b9754c309ee23687be592bc0b9
Guilherme Chaddad <guichaddad@gmail.com>
2018-10-25 19:41:20 -0300

    Kotlin Lecture 80

---
+++ Kotlin/CourseProject/app/src/main/java/com/example/courseandroidapp/HabitsAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.courseandroidapp
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.single_card.view.*
+
+class HabitsAdapter(val habits: List<Habit>) : RecyclerView.Adapter<HabitsAdapter.HabitViewHolder> () {
+
+    class HabitViewHolder(val card: View) : RecyclerView.ViewHolder(card)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_card, parent, false)
+
+        return HabitViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HabitViewHolder, index: Int) {
+        holder.card.tv_title.text = habits[index].title
+        holder.card.tv_description.text = habits[index].description
+        holder.card.iv_icon.setImageResource(habits[index].image)
+    }
+
+    override fun getItemCount() = habits.size
+
+}
--- Kotlin/CourseProject/app/src/main/java/com/example/courseandroidapp/MainActivity.kt
+++ Kotlin/CourseProject/app/src/main/java/com/example/courseandroidapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.courseandroidapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        rv.setHasFixedSize(true)
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = HabitsAdapter(getSampleHabits())
     }
 }
