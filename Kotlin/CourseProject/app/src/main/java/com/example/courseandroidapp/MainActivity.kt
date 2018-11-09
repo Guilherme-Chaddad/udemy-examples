3c40c1e744b2dfadf1f2b18bcad834a05402b25a
e078846 <guilherme.rufino@mastercard.com>
2018-11-09 07:29:44 -0600

    Kotlin Lecture 83

--- Kotlin/CourseProject/app/src/main/java/com/example/courseandroidapp/MainActivity.kt
+++ Kotlin/CourseProject/app/src/main/java/com/example/courseandroidapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.courseandroidapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +18,18 @@ class MainActivity : AppCompatActivity() {
         rv.setHasFixedSize(true)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = HabitsAdapter(getSampleHabits())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.add_habit) {
+            val intent = Intent(this, CreateHabitActivity::class.java)
+            startActivity(intent)
+        }
+        return true
     }
 }
